d2fa866dcc93240d091d2f0ffb06841753628ecf
cristiandonosoc <cristiandonosoc@gmail.com>
2018-04-23 23:07:42 -0700

    Initial working compiling

---
+++ src/main.cc
@@ -0,0 +1,15 @@
+/******************************************************************************
+ * @file: main.cc
+ * @author: Cristián Donoso C.
+ * @email: cristiandonosoc@gmail.com
+ * @date: 2018-04-23
+ * @license: 2018 Cristián Donoso C. - All Rights Reserved.
+ *
+ * @description: TODO(Cristian): Add description
+ ******************************************************************************/
+
+#include <cstdio>
+
+int main() {
+  printf("Hello, World!\n");
+}
