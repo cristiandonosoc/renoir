735524d5babc78a3fa43253adeccbdea077cf7c5
cristiandonosoc <cristiandonosoc@gmail.com>
2018-04-23 22:46:45 -0700

    Initial port of useful bits from Picasso

---
+++ .set_env.bat
@@ -0,0 +1,3 @@
+@echo off
+set SDL2=C:\Users\cristiandonosoc\Libraries\SDL2-2.0.7
+call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x64
---
+++ .set_env.sh
@@ -0,0 +1,1 @@
+export SDL2=C:\Users\cristiandonosoc\Libraries\SDL2-2.0.7
