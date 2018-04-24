dcad9485c7c4eec314021a4316130d03f66c6827
cristiandonosoc <cristiandonosoc@gmail.com>
2018-04-23 23:33:31 -0700

    Created simplified frame loop

--- src/main.cc
+++ src/main.cc
@@ -12,6 +12,9 @@
 #include <external/GL/gl3w.h>
 #include <SDL.h>
 
+#include <imgui/imgui.h>
+#include <external/imgui_impl_sdl_gl3.h>
+
 
 // TODO(Cristian): Move this to platform
 #ifdef _WIN32
@@ -48,13 +51,19 @@ int main(int, char**) {
   SDL_GL_SetSwapInterval(1);  // Enable vsync
   gl3wInit();
 
+  ImGui::CreateContext();
+  ImGuiIO& io = ImGui::GetIO();
+  ImGui::StyleColorsDark();
+  // Mainly sets up the HDC and SDL Keyboard/Mouse stuf
+  ImGui_ImplSdlGL3_Init(window);
+
 #ifdef _WIN32
   // TODO(Cristian): For some reason, we need this for stdout logging in windows
   // TODO(Cristian): Move this to the platform layer
   fflush(stderr);
 
   // Maximize the window
-  /* ShowWindow((HWND)io.ImeWindowHandle, SW_MAXIMIZE); */
+  ShowWindow((HWND)io.ImeWindowHandle, SW_MAXIMIZE);
 #endif
 
   fprintf(stderr, "OpenGL Vendor: %s", glGetString(GL_VENDOR));
@@ -63,6 +72,39 @@ int main(int, char**) {
   fprintf(stderr, "OpenGL Shading Language Version: %s",
               glGetString(GL_SHADING_LANGUAGE_VERSION));
   fprintf(stderr, "OpenGL Extension: %s", glGetString(GL_EXTENSIONS));
+
+
+  bool done = false;
+  while (!done) {
+      // You can read the io.WantCaptureMouse, io.WantCaptureKeyboard flags to tell if dear imgui wants to use your inputs.
+      // - When io.WantCaptureMouse is true, do not dispatch mouse input data to your main application.
+      // - When io.WantCaptureKeyboard is true, do not dispatch keyboard input data to your main application.
+      // Generally you may always pass all inputs to dear imgui, and hide them from your application based on those two flags.
+      SDL_Event event;
+      while (SDL_PollEvent(&event))
+      {
+          ImGui_ImplSdlGL3_ProcessEvent(&event);
+          if (event.type == SDL_QUIT)
+              done = true;
+      }
+      ImGui_ImplSdlGL3_NewFrame(window);
+
+			if (io.KeysDown[io.KeyMap[ImGuiKey_Escape]]) {
+				done = true;
+			}
+
+
+      static bool show_demo = true;
+      ImGui::ShowDemoWindow(&show_demo);
+
+      glViewport(0, 0, (int)ImGui::GetIO().DisplaySize.x, (int)ImGui::GetIO().DisplaySize.y);
+      glClear(GL_COLOR_BUFFER_BIT);
+
+
+      ImGui::Render();
+      ImGui_ImplSdlGL3_RenderDrawData(ImGui::GetDrawData());
+      SDL_GL_SwapWindow(window);
+  }
 
 
   return 0;
